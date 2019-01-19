e3857b7ab7c5e7d7d440e0ab1f89039da06e202c
Kolachow <kolas3000@gmail.com>
2019-01-19 14:56:58 +0100

    JS1

---
+++ playground.js
@@ -0,0 +1,9 @@
+// console.log("Koleś Git".toUpperCase());
+// console.log("JavaScript jest mocno typowanym językiem".replace("mocno", "słabo"));
+// console.log("JavaScript jest językiem wysokiego poziomu".split(" "));
+// console.log("Jestem królem front-end'u. ".repeat(3));
+
+console.log((234).toString(2));
+console.log(23.2345432.toFixed(3));
+console.log(parseFloat("54.34test"));
+console.log(isFinite(2e134));
